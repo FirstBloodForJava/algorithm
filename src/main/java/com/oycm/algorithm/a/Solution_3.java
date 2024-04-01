e3b94b51025eeb5552579086fdcf4d411ebdba8b
ouyangcm <mingorg@163.com>
2024-04-01 16:57:32 +0800

    a.三数之和

--- src/main/java/com/oycm/algorithm/a/Solution_3.java
+++ src/main/java/com/oycm/algorithm/a/Solution_3.java
@@ -1,8 +1,15 @@
 package com.oycm.algorithm.a;
 
+import java.util.List;
+
 /**
  * @author ouyangcm
  * create 2024/4/1 16:15
  */
 public class Solution_3 {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        return null;
+    }
 }
