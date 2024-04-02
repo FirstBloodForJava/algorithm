364d98ed1aa1d29bd5bb99cd81f9d8b16b7225be
oycm <1164864987@qq.com>
2024-04-02 21:38:05 +0800

    b.暴力枚举+部分优化

--- src/main/java/com/oycm/algorithm/b/Solution_1.java
+++ src/main/java/com/oycm/algorithm/b/Solution_1.java
@@ -1,4 +1,38 @@
 package com.oycm.algorithm.b;
 
 public class Solution_1 {
+
+    public static void main(String[] args) {
+        int[] ints = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int result = maxArea(ints);
+        System.out.println(result);
+    }
+
+    public static int maxArea(int[] height) {
+
+        return method_1(height);
+    }
+
+    // 暴力枚举+一点优化 时间复杂度n^2
+    public static int method_1(int[] height){
+        int result = 0;
+        for (int i = 0; i < height.length-1; i++) {
+            int right = height.length-1;
+            while (i < right){
+                if (height[right] >= height[i]){
+                    result = Math.max(result, height[i]*(right-i));
+                    break;
+                }else {
+                    result = Math.max(result,height[right]*(right-i));
+                    right--;
+                    while (i < right && height[right]<=height[right+1]){
+                       right--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
 }
