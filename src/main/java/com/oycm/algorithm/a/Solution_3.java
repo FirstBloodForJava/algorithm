dd15fb2ed79e45347b06c8e437e9d321d23d8234
ouyangcm <mingorg@163.com>
2024-04-02 17:27:57 +0800

    a.三数之和

--- src/main/java/com/oycm/algorithm/a/Solution_3.java
+++ src/main/java/com/oycm/algorithm/a/Solution_3.java
@@ -1,5 +1,7 @@
 package com.oycm.algorithm.a;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,5 +13,18 @@ public class Solution_3 {
     public List<List<Integer>> threeSum(int[] nums) {
 
         return null;
+    }
+
+    public static List<List<Integer>> method_1(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+
+        // 对数组排序 对于 i,j,k,存在nums[i]+nums[j]+nums[k]=0 则nums[i] <=0;nums[j]+nums[k]>=0且nums[j]+nums[k]=-nums[i];
+        Arrays.sort(nums);
+
+
+
+
+
+        return result;
     }
 }
