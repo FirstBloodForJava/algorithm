c2fdd74c2a8f6dc667c8f378ec69b182409c6629
ouyangcm <mingorg@163.com>
2024-04-10 17:28:18 +0800

    c.暴力解法

--- src/main/java/com/oycm/algorithm/c/Solution_1.java
+++ src/main/java/com/oycm/algorithm/c/Solution_1.java
@@ -2,8 +2,33 @@ package com.oycm.algorithm.c;
 
 public class Solution_1 {
 
-    public int minSubArrayLen(int target, int[] nums) {
+    public static void main(String[] args) {
+        Solution_1 solution = new Solution_1();
+        int[] nums = {2,3,1,2,4,3};
+        System.out.println(solution.minSubArrayLen(7, nums));
+    }
 
-        return 0;
+    public int minSubArrayLen(int target, int[] nums) {
+        return method_1(target, nums);
+    }
+
+    public int method_1(int target, int[] nums){
+        int result = nums.length + 1;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum >= target) {
+                    result = Math.min(result, j-i+1);
+                    break;
+                }
+            }
+            if (i == 0 && target > sum){
+                break;
+            }
+            sum = 0;
+        }
+
+        return result > nums.length ? 0 : result;
     }
 }
