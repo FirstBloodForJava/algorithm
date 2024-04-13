d6ed79b43a0043e58866facedabbd4fe1a5a6adb
oycm <1164864987@qq.com>
2024-04-13 22:34:02 +0800

    f.206.反转链表

--- src/main/java/com/oycm/algorithm/f/Solution_1.java
+++ src/main/java/com/oycm/algorithm/f/Solution_1.java
@@ -3,8 +3,27 @@ package com.oycm.algorithm.f;
 import com.oycm.algorithm.ListNode;
 
 public class Solution_1 {
+
+    public static void main(String[] args) {
+        ListNode l3 = new ListNode(3);
+        ListNode l2 = new ListNode(2, l3);
+        ListNode l1 = new ListNode(1, l2);
+        Solution_1 solution = new Solution_1();
+        ListNode result = solution.reverseList(l1);
+        System.out.println(result.val);
+    }
+
     public ListNode reverseList(ListNode head) {
-        return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode result = new ListNode(head.val);
+        ListNode temp = head;
+        while ((temp = temp.next) != null){
+            result = new ListNode(temp.val, result);
+        }
+
+        return result;
     }
 }
 
