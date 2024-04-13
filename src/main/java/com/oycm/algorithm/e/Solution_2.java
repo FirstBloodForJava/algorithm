4f9057573090d30c5aeb79e34a9d08c6d24dcc8b
oycm <1164864987@qq.com>
2024-04-13 22:18:22 +0800

    e.153.寻找旋转排序数组中的最小值优化

--- src/main/java/com/oycm/algorithm/e/Solution_2.java
+++ src/main/java/com/oycm/algorithm/e/Solution_2.java
@@ -3,9 +3,11 @@ package com.oycm.algorithm.e;
 public class Solution_2 {
 
     public static void main(String[] args) {
-        int[] nums = {2, 1};
+        int[] nums = {3,4,5,1,2};
         Solution_2 solution = new Solution_2();
         System.out.println(solution.findMin(nums));
+        System.out.println(solution.method_2(nums));
+        System.out.println(solution.method_3(nums));
     }
 
     public int findMin(int[] nums) {
@@ -34,5 +36,34 @@ public class Solution_2 {
         }
 
         return Math.min(nums[left], nums[right]);
+    }
+
+    public int method_2(int[] nums) {
+        int left = -1;
+        int right = nums.length - 1;
+        while (left+1 <right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] < nums[nums.length-1]) {
+                right = mid;
+            }else {
+                left = mid;
+            }
+        }
+        return nums[right];
+    }
+
+    public int method_3(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] > nums[nums.length-1]){
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+
+        return nums[left];
     }
 }
