64fb9a833c31497416466f4a5fb70265cbc518eb
oycm <1164864987@qq.com>
2024-04-14 17:13:49 +0800

    f.92.反转链表 II+空间优化

--- src/main/java/com/oycm/algorithm/f/Solution_2.java
+++ src/main/java/com/oycm/algorithm/f/Solution_2.java
@@ -87,4 +87,23 @@ public class Solution_2 {
 
         return result;
     }
+
+    public ListNode method_2(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left-1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next = null;
+        for (int i = 0; i < right-left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+
+        return dummyNode.next;
+    }
 }
