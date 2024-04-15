9ee65011042525b474372868302ad9b793dd4a59
ouyangcm <mingorg@163.com>
2024-04-15 17:10:57 +0800

    g.142.环形链表 II-hash表

--- src/main/java/com/oycm/algorithm/g/Solution_3.java
+++ src/main/java/com/oycm/algorithm/g/Solution_3.java
@@ -2,8 +2,33 @@ package com.oycm.algorithm.g;
 
 import com.oycm.algorithm.ListNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution_3 {
+
+    /**
+     *
+     * @param head 链表的头节点
+     * @return 环形链表则是尾节点指向的节点, 非环形则节点的值为-1
+     */
     public ListNode detectCycle(ListNode head) {
         return null;
+    }
+    public ListNode method_1(ListNode head) {
+        ListNode result = null;
+        ListNode curr = head;
+        Map<Integer,ListNode> map = new HashMap<>();
+        while (curr != null){
+            if (!map.containsKey(curr.hashCode())) {
+                map.put(curr.hashCode(), curr);
+            }else {
+                result = map.get(curr.hashCode());
+                break;
+            }
+            curr = curr.next;
+        }
+
+        return result;
     }
 }
