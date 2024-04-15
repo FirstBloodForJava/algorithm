cc79e258272b32b72acaf48bc238e3295882d01c
oycm <1164864987@qq.com>
2024-04-15 21:04:39 +0800

    g.143.重排链表-中间节点+反转链表+指针切换

--- src/main/java/com/oycm/algorithm/g/Solution_4.java
+++ src/main/java/com/oycm/algorithm/g/Solution_4.java
@@ -6,8 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution_4 {
-    public void reorderList(ListNode head) {
 
+    public static void main(String[] args) {
+        Solution_4 solution = new Solution_4();
+        ListNode head = ListNode.getHead(10);
+        System.out.println(head);
+        solution.method_2(head);
+        System.out.println(head);
+
+    }
+
+    public void reorderList(ListNode head) {
+        method_1(head);
     }
     public void method_1(ListNode head) {
         if (head == null) {
@@ -30,5 +40,53 @@ public class Solution_4 {
             j--;
         }
         list.get(i).next = null;
+    }
+
+    /**
+     * 寻找中间节点
+     * @param head
+     * @return
+     */
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 反转链表
+     * @param head
+     * @return
+     */
+    public ListNode reverseNode(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+    public void method_2(ListNode head) {
+        // 寻找链表的中间节点
+        ListNode middleNode = middleNode(head);
+        // 将中间节点之后的节点反转
+        ListNode rightHead = reverseNode(middleNode);
+
+        while (rightHead.next != null) {
+            ListNode leftNext = head.next;
+            ListNode rightNext = rightHead.next;
+            head.next = rightHead;
+            rightHead.next = leftNext;
+            head = leftNext;
+            rightHead = rightNext;
+        }
+
     }
 }
