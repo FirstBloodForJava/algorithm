a762fc3059f83c55e7e525c7f47d2548cdb5f503
ouyangcm <mingorg@163.com>
2024-04-15 16:10:49 +0800

    g.141.环形链表-快慢指针

--- src/main/java/com/oycm/algorithm/g/Solution_2.java
+++ src/main/java/com/oycm/algorithm/g/Solution_2.java
@@ -3,7 +3,24 @@ package com.oycm.algorithm.g;
 import com.oycm.algorithm.ListNode;
 
 public class Solution_2 {
+
+    public static void main(String[] args) {
+        Solution_2 solution = new Solution_2();
+        System.out.println(solution.hasCycle(null));
+    }
+
     public boolean hasCycle(ListNode head) {
-        return false;
+        boolean flag = false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
     }
 }
