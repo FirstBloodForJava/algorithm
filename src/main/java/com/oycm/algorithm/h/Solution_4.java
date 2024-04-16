a786666fb23cb2469a1860a063630e56c91ab071
oycm <1164864987@qq.com>
2024-04-16 20:34:33 +0800

    h.82.删除排序链表中的重复元素II: 删除重复节点并记录，再遍历删除记录的节点

--- src/main/java/com/oycm/algorithm/h/Solution_4.java
+++ src/main/java/com/oycm/algorithm/h/Solution_4.java
@@ -2,6 +2,9 @@ package com.oycm.algorithm.h;
 
 import com.oycm.algorithm.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution_4 {
 
     /**
@@ -13,4 +16,33 @@ public class Solution_4 {
     public ListNode deleteDuplicates(ListNode head) {
         return null;
     }
-}
+
+    public ListNode method_1(ListNode head) {
+        if (head == null) return head;
+
+        Set<Integer> set = new HashSet<>();
+        // 先删除重复节点，并且记录重复的节点
+        ListNode curr = head;
+        while (curr.next != null) {
+            if (curr.val == curr.next.val) {
+                set.add(curr.val);
+                curr.next = curr.next.next;
+            }else {
+                curr = curr.next;
+            }
+        }
+        // 遍历无重复的节点，节点有删除记录，则删除节点
+        ListNode dummy = new ListNode(-1, head);
+        curr = dummy;
+        while (curr.next != null) {
+            // 之前被删除过
+            if (set.contains(curr.next.val)) {
+                curr.next = curr.next.next;
+            }else {
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+    }
+ }
