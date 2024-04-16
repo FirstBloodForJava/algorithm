634d18e53e67e939bc5f479da4fbfab77cfe6736
oycm <1164864987@qq.com>
2024-04-16 20:01:51 +0800

    h.19.删除链表的倒数第N个结点: 计算链表长度/双指针

--- src/main/java/com/oycm/algorithm/h/Solution_2.java
+++ src/main/java/com/oycm/algorithm/h/Solution_2.java
@@ -68,4 +68,55 @@ public class Solution_2 {
 
         return pre;
     }
+
+    /**
+     * 创建一个dummy节点(哨兵节点),为了删除n=L的情况,知道链表的长度L,删除倒数第n个节点即从哨兵节点开始遍历i=1,当i=L-n+1时,下一个节点就是要删除的节点
+     * eg: 0(哨兵) -> 1 -> 2 -> 3 -> 4,删除第4个,从dummy开始,i=1,即dummy.next节点删除
+     * eg: 0(哨兵) -> 1 -> 2 -> 3 -> 4,删除第3个节点,从dummy开始i=1,i=2,指向1,下一个节点2就是要删除的
+     * @param head 链表的长度
+     * @param n 从后面数的第n个节点
+     * @return 删除后的链表
+     */
+    public ListNode method_1(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+
+        ListNode temp = head;
+        int length = 0;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        ListNode curr = dummy;
+        for (int i = 1; i < length-n+1; i++) {
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+
+        return dummy.next;
+    }
+
+    /**
+     * 创建一个dummy节点，右节点从dummy开始，让其向右移动n次，再指定一个left节点指向dummy，这个时候left节点和right相距n，但是left是right的倒数第n+1节点
+     * 当right节点移动到未节点时,即right.next==null，left的下一个节点就是要删除的节点
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode method_2(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode right = dummy;
+        while (n > 0) {
+            n--;
+            right = right.next;
+        }
+        ListNode left = dummy;
+        while (right.next != null) {
+            left = left.next;
+            right = right.next;
+        }
+        // left下一个节点要被删除
+        left.next = left.next.next;
+
+        return dummy.next;
+    }
 }
