31ae6e00c3d32747fa5731413cb68418fe17119d
oycm <1164864987@qq.com>
2024-04-17 20:38:53 +0800

    i.104.二叉树的最大深度

--- src/main/java/com/oycm/algorithm/i/Solution_1.java
+++ src/main/java/com/oycm/algorithm/i/Solution_1.java
@@ -5,13 +5,36 @@ import com.oycm.algorithm.TreeNode;
 
 public class Solution_1 {
 
+    public static void main(String[] args) {
+        Solution_1 solution = new Solution_1();
+        TreeNode root = new TreeNode(1);
+        TreeNode left1 = new TreeNode(2);
+        TreeNode right1 = new TreeNode(3);
+        root.left = left1;
+        root.right = right1;
+
+        TreeNode left2 = new TreeNode(4);
+        TreeNode right2 = new TreeNode(5);
+        left1.left = left2;
+        right1.right = right2;
+        System.out.println(solution.maxDepth(root));
+
+    }
+
     /**
      * 二叉树的最大深度
      * https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
+     * 在知道左子树和右子树的最大深度l和r时，那么二叉树的深度就是max(l,r)+1
+     * 左子树和右子树的最大深度有可以用同样的方式进行计算
      * @param root
      * @return
      */
     public int maxDepth(TreeNode root) {
-        return 1;
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
     }
 }
