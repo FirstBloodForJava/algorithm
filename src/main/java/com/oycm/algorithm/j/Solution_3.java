615959f13c037a0889335c8a8e4cf5eb035d986e
ouyangcm <mingorg@163.com>
2024-04-18 15:31:36 +0800

    平衡二叉树

--- src/main/java/com/oycm/algorithm/j/Solution_3.java
+++ src/main/java/com/oycm/algorithm/j/Solution_3.java
@@ -5,7 +5,7 @@ import com.oycm.algorithm.TreeNode;
 public class Solution_3 {
 
     /**
-     * 平衡二叉树
+     * 平衡二叉树 是指该树所有节点的左右子树的深度相差不超过 1
      * https://leetcode.cn/problems/balanced-binary-tree/description/
      * @param root
      * @return
