688369ce8c7a05bbfbb9cb409ca6ee2af02c8bca
ouyangcm <mingorg@163.com>
2024-04-25 11:29:13 +0800

    k.二叉搜索树

--- src/main/java/com/oycm/algorithm/App.java
+++ src/main/java/com/oycm/algorithm/App.java
@@ -1,5 +1,8 @@
 package com.oycm.algorithm;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @author ouyangcm
  * create 2024/4/1 13:35
@@ -7,6 +10,11 @@ package com.oycm.algorithm;
 public class App {
 
     public static void main(String[] args) {
-
+        ThreadPoolExecutor executorPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+//        executorPool.execute(()->{
+//            System.out.println(Thread.currentThread().getName());
+//        });
+        System.out.println(1 << 32);
+        System.out.println(1 | 1);
     }
 }
--- src/main/java/com/oycm/algorithm/k/Solution_1.java
+++ src/main/java/com/oycm/algorithm/k/Solution_1.java
@@ -4,6 +4,14 @@ import com.oycm.algorithm.TreeNode;
 
 public class Solution_1 {
 
+    /**二叉搜索树定义：
+     * 节点的左子树只包含 小于 当前节点的数
+     * 节点的右子树只包含 大于 当前节点的数
+     * 所有左子树和右子树自身必须也是二叉搜索树
+     * https://leetcode.cn/problems/validate-binary-search-tree/
+     * @param root
+     * @return
+     */
     public boolean isValidBST(TreeNode root) {
         return false;
     }
