9413eecd922e38a65da0562827f59a5651a60034
ouyangcm <mingorg@163.com>
2024-04-25 13:16:32 +0800

    k.98.二叉搜索树-前序遍历

--- src/main/java/com/oycm/algorithm/k/Solution_1.java
+++ src/main/java/com/oycm/algorithm/k/Solution_1.java
@@ -5,14 +5,32 @@ import com.oycm.algorithm.TreeNode;
 public class Solution_1 {
 
     /**二叉搜索树定义：
-     * 节点的左子树只包含 小于 当前节点的数
-     * 节点的右子树只包含 大于 当前节点的数
+     * 节点的左子树(所有)只包含 小于 当前节点的数
+     * 节点的右子树(所有)只包含 大于 当前节点的数
      * 所有左子树和右子树自身必须也是二叉搜索树
      * https://leetcode.cn/problems/validate-binary-search-tree/
      * @param root
      * @return
      */
     public boolean isValidBST(TreeNode root) {
+
+        boolean preorder = method_1(root, Long.MIN_VALUE, Long.MAX_VALUE);
+
         return false;
+    }
+
+    /**
+     * 前序遍历，先比较后递归，递归需要知道的边界传递给它
+     * @param node
+     * @param min
+     * @param max
+     * @return
+     */
+    private boolean method_1(TreeNode node, long min, long max) {
+        if (node == null) return true;
+
+        if (node.val <= min || node.val >= max) return false;
+
+        return method_1(node.left, min, node.val) && method_1(node.right, node.val, max);
     }
 }
