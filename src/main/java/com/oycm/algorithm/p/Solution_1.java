031359ba37bb2d13b24da15a69dc82adee823529
oycm <1164864987@qq.com>
2024-04-27 18:30:35 +0800

    p.回溯-排列型

---
+++ src/main/java/com/oycm/algorithm/p/Solution_1.java
@@ -0,0 +1,16 @@
+package com.oycm.algorithm.p;
+
+import java.util.List;
+
+public class Solution_1 {
+
+    /**
+     * 46.全排列
+     * https://leetcode.cn/problems/permutations/
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        return null;
+    }
+}
---
+++ src/main/java/com/oycm/algorithm/p/Solution_2.java
@@ -0,0 +1,16 @@
+package com.oycm.algorithm.p;
+
+import java.util.List;
+
+public class Solution_2 {
+
+    /**
+     * 51.N皇后
+     * https://leetcode.cn/problems/n-queens/
+     * @param n
+     * @return
+     */
+    public List<List<String>> solveNQueens(int n) {
+        return null;
+    }
+}
