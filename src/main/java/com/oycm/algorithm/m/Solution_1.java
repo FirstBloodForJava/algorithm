404d52ea3234b2923dfb783dcde437c03286e60b
oycm <1164864987@qq.com>
2024-04-28 21:50:44 +0800

    m.102.二叉树的层序遍历-两个数组

--- src/main/java/com/oycm/algorithm/m/Solution_1.java
+++ src/main/java/com/oycm/algorithm/m/Solution_1.java
@@ -18,6 +18,11 @@ public class Solution_1 {
         return result;
     }
 
+    public List<List<Integer>> method_1(TreeNode root) {
+        deep(root, 0);
+        return result;
+    }
+
     List<List<Integer>> result = new ArrayList<>();
     public void deep(TreeNode node, int height) {
         if (node == null) return;
@@ -30,5 +35,31 @@ public class Solution_1 {
         }
         deep(node.left, height+1);
         deep(node.right, height+1);
+    }
+
+    /**
+     * 双数组层序遍历
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> method_2(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        List<TreeNode> cur = new ArrayList<>();
+        if (root != null) cur.add(root);
+
+        while (!cur.isEmpty()) {
+            List<TreeNode> nex = new ArrayList<>();
+            List<Integer> list = new ArrayList<>();
+            for (TreeNode node : cur) {
+                list.add(node.val);
+                if (node.left != null) nex.add(node.left);
+                if (node.right != null) nex.add(node.right);
+            }
+            cur = nex;
+            ans.add(list);
+        }
+
+        return ans;
     }
 }
