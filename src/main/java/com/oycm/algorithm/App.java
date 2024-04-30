bb3b4c0df56bb6fd305d9fe45642ec2524676ead
ouyangcm <mingorg@163.com>
2024-04-30 16:00:47 +0800

    n.回溯-子集型-131.分割回文串-选/不选

--- src/main/java/com/oycm/algorithm/App.java
+++ src/main/java/com/oycm/algorithm/App.java
@@ -1,8 +1,5 @@
 package com.oycm.algorithm;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-
 /**
  * @author ouyangcm
  * create 2024/4/1 13:35
@@ -10,12 +7,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class App {
 
     public static void main(String[] args) {
-        ThreadPoolExecutor executorPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
-//        executorPool.execute(()->{
-//            System.out.println(Thread.currentThread().getName());
-//        });
-        System.out.println(1 << 32);
-        System.out.println(1 | 1);
 
     }
 }
