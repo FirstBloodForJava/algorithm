672ebcc01c2f1dcf38f629d33bd6aa726b49968c
ouyangcm <mingorg@163.com>
2024-05-06 17:04:06 +0800

    r.0-1背包问题-r.目标和

--- src/main/java/com/oycm/algorithm/r/Solution_1.java
+++ src/main/java/com/oycm/algorithm/r/Solution_1.java
@@ -12,4 +12,35 @@ public class Solution_1 {
     public int findTargetSumWays(int[] nums, int target) {
         return 1;
     }
+
+
+    /**
+     * dfs暴力解法
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int method_1(int[] nums, int target) {
+        dfs_1(nums, target, 0);
+        return count;
+    }
+
+    int count = 0;
+    private void dfs_1(int[] nums, int target, int i) {
+        if (nums.length == i) {
+            if (target == 0) {
+                count++;
+            }
+            return;
+        }
+        dfs_1(nums, target-nums[i],i+1);
+        dfs_1(nums, target+nums[i],i+1);
+
+    }
+
+    public static void main(String[] args) {
+        Solution_1 solution = new Solution_1();
+        int[] nums = {1, 1, 1, 1, 1};
+        System.out.println(solution.method_1(nums,3));
+    }
 }
