009aab4fdbd68d2c1218a209ff500fc443cb4cab
oycm <1164864987@qq.com>
2024-05-07 21:29:21 +0800

    r.01背包-494.目标和:回溯+记忆集搜索

--- src/main/java/com/oycm/algorithm/r/Solution_1.java
+++ src/main/java/com/oycm/algorithm/r/Solution_1.java
@@ -38,6 +38,57 @@ public class Solution_1 {
 
     }
 
+    private int method_1_1(int[] nums, int target) {
+        for (int i : nums) {
+            target += i;
+        }
+        if (target < 0 || target % 2 != 0) {
+            return 0;
+        }
+        target /= 2;
+        int[][] dp = new int[nums.length][target+1];
+        return dfs_1(nums, target, nums.length-1, dp);
+    }
+
+    private int dfs_1(int[] nums, int target, int i, int[][] dp) {
+        if (i < 0) {
+            if (target == 0 ) return 1;
+            return 0;
+        }
+        if (target < nums[i]) {
+            dp[i][target] = dfs_1(nums, target, i-1, dp);
+        }else {
+            dp[i][target] = dfs_1(nums, target, i-1, dp) + dfs_1(nums, target-nums[i], i-1, dp);
+        }
+        return dp[i][target];
+    }
+
+    /**
+     * 假设选为正数的和为: p
+     * 假设nums的数组和为: s
+     * 则选为负数正数和为: s-p
+     * 则有: p - (s-p) = target
+     * p = (s + target)/2,由于p是>=0(所选正数的和)的数，所有s+target肯定非负偶数
+     * 所以问题转换成了：求选的正数和为p的组合数(方案数量)
+     * 也可以理解为背包问题: 从[0-n]的物品中，物品价值为nums[i]选出物品价值总和等于target的组合数
+     * 定义一个二维数组dp[i][j] i表示从[0-i]选任意数,使得和为j的对应的组合数
+     * @param nums 正数数组
+     * @param target 目标和
+     * @return 为目标和的所有组合数
+     */
+    public int method_2(int[] nums, int target) {
+        for (int i : nums) {
+            target += i;
+        }
+        if (target < 0 || target % 2 != 0) {
+            return 0;
+        }
+        target /= 2;
+
+
+        return 1;
+    }
+
     public static void main(String[] args) {
         Solution_1 solution = new Solution_1();
         int[] nums = {1, 1, 1, 1, 1};
