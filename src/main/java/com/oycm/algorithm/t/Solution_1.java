4d9f46d7c0838deac4112b7f8772023a9dcd18e5
ouyangcm <mingorg@163.com>
2024-05-11 17:22:43 +0800

    t.最长递增子序列-回溯

--- src/main/java/com/oycm/algorithm/t/Solution_1.java
+++ src/main/java/com/oycm/algorithm/t/Solution_1.java
@@ -18,40 +18,57 @@ public class Solution_1 {
 
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, i);
+            ans = Math.max(ans, dfs(nums, i));
         }
         return ans;
     }
 
     private int dfs(int[] nums, int i) {
-        if (i == 0) {
-            return 1;
-        }
-        for (int j = 0; j < i; j++) {
+        int res = 0;
 
+        for (int j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                res = Math.max(res, dfs(nums, j));
+            }
         }
-        return 0;
+        return res + 1;
     }
 
     private int method_2(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n+1][n+1];
+        int[] dp = new int[n];
+        int ans = 0;
+        for (int i = 0; i < n ; i++) {
+            ans = Math.max(ans, dfs(nums, i, dp));
+        }
+        return ans;
 
-        for (int i = 1; i <= n ; i++) {
-            for (int j = 1; j <= n; j++) {
+    }
+    private int dfs(int[] nums, int i, int[] dp) {
+        if (dp[i] > 0) return dp[i];
 
+        for (int j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dfs(nums, j, dp));
             }
         }
-        return dp[n][n];
 
+        return ++dp[i];
     }
 
 
+    public static int method() {
+        int[] i = {1};
+        ++i[0];
+        return i[0]+1;
+    }
 
     public static void main(String[] args) {
         Solution_1 solution = new Solution_1();
         int[] nums = {4,10,4,3,8,9};
         System.out.println(solution.method_1(nums));
+        System.out.println(solution.method_2(nums));
 
+        System.out.println(method());
     }
 }
