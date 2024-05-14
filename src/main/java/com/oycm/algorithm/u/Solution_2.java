fe9625a3dacae972e42f69bd9deb4da48af4dbfb
oycm <1164864987@qq.com>
2024-05-14 21:25:17 +0800

    t-309.买卖股票的最佳时机含冷冻期: 动态规划

--- src/main/java/com/oycm/algorithm/u/Solution_2.java
+++ src/main/java/com/oycm/algorithm/u/Solution_2.java
@@ -36,4 +36,17 @@ public class Solution_2 {
 
         return dp[i][flag];
     }
+
+    // 动态规划
+    public int method_2(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n+2][2];
+        // dfs(-1,1) = -∞
+        dp[1][1] = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            dp[i+2][0] = Math.max(dp[i+1][0], dp[i+1][1] + prices[i]);
+            dp[i+2][1] = Math.max(dp[i+1][1], dp[i][0] - prices[i]);
+        }
+        return dp[n+1][0];
+    }
 }
