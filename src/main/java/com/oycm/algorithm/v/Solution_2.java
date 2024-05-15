59e74762467703e0ce5115a50f859ba68358e86c
oycm <1164864987@qq.com>
2024-05-15 20:33:23 +0800

    v-1039.多边形三角剖分的最低得分: 回溯

--- src/main/java/com/oycm/algorithm/v/Solution_2.java
+++ src/main/java/com/oycm/algorithm/v/Solution_2.java
@@ -1,14 +1,36 @@
 package com.oycm.algorithm.v;
 
+import java.util.Arrays;
+
 public class Solution_2 {
 
     /**
-     * 1039
+     * 1039.多边形三角剖分的最低得分
      * https://leetcode.cn/problems/minimum-score-triangulation-of-polygon/
      * @param values
      * @return
      */
     public int minScoreTriangulation(int[] values) {
         return 0;
+    }
+
+    public int method_1(int[] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+        for (int[] ints : dp) {
+            Arrays.fill(ints, -1);
+        }
+        return dfs(0, n-1, values, dp);
+    }
+
+    public int dfs(int i, int j, int[] values, int[][] dp) {
+        if (i+1 == j) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        int temp = Integer.MAX_VALUE;
+        for (int k = i+1; k < j; k++) {
+            temp = Math.min(temp, dfs(i, k, values, dp) + dfs(k, j, values, dp) + values[i]*values[j]*values[k]);
+        }
+        dp[i][j] = temp;
+        return dp[i][j];
     }
 }
