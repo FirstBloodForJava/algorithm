44627acd7789aa8a60797d2c0dea3a20fa4bdc34
ouyangcm <mingorg@163.com>
2024-05-15 11:14:19 +0800

    u-恰好交易k次的最大收益：递推实现

--- src/main/java/com/oycm/algorithm/u/Solution_4.java
+++ src/main/java/com/oycm/algorithm/u/Solution_4.java
@@ -1,5 +1,7 @@
 package com.oycm.algorithm.u;
 
+import java.util.Arrays;
+
 /**
  * @author ouyangcm
  * create 2024/5/15 10:17
@@ -42,9 +44,39 @@ public class Solution_4 {
         return Math.max(dfs(k, i-1, 0, prices), dfs(k, i-1, 1, prices) + prices[i]);
     }
 
+    // 递推实现
+    public int method_2(int k, int[] prices) {
+        int min = -1000000;
+        if (k <= 0 ) return 0;
+        // 交易次数比股票的天数/2还大
+        if (k >= prices.length/2 + 1) return 0;
+        int n = prices.length;
+        int[][][] dp = new int[n+1][k+2][2];
+        // 初始化
+        // dp[i][0][0] = min
+        // dp[0][k][f] = min (k>=1)
+        // dp[0][1][0] = 0;
+        // dp[0][1][1] = min;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k+2; j++) {
+                Arrays.fill(dp[i][j], min);
+            }
+        }
+        dp[0][1][0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < k+2; j++) {
+                dp[i+1][j][0] = Math.max(dp[i][j][0], dp[i][j-1][1] + prices[i]);
+                dp[i+1][j][1] = Math.max(dp[i][j][1], dp[i][j][0] - prices[i]);
+            }
+        }
+
+        return dp[n][k+1][0];
+    }
+
     public static void main(String[] args) {
         Solution_4 solution = new Solution_4();
         int[] prices = {3, 2, 6, 5, 0, 3};
-        System.out.println(solution.method_1(4, prices));
+        System.out.println(solution.method_1(2, prices));
+        System.out.println(solution.method_2(2, prices));
     }
 }
