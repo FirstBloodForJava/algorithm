997d861d9efcff3a0435648db2f969e246dd8408
oycm <1164864987@qq.com>
2024-05-17 21:54:37 +0800

    w-543.二叉树的直径: 递归

--- src/main/java/com/oycm/algorithm/w/Solution_1.java
+++ src/main/java/com/oycm/algorithm/w/Solution_1.java
@@ -5,12 +5,28 @@ import com.oycm.algorithm.TreeNode;
 public class Solution_1 {
 
     /**
-     * 543
+     * 543.二叉树的直径
      * https://leetcode.cn/problems/diameter-of-binary-tree/
      * @param root
      * @return
      */
     public int diameterOfBinaryTree(TreeNode root) {
-        return 0;
+        dfs(root);
+        return ans;
+    }
+    int ans = 0;
+
+    public int dfs(TreeNode node) {
+        if (node == null) return -1;
+
+        int left = dfs(node.left) + 1;
+        int right = dfs(node.right) + 1;
+        ans = Math.max(ans, left+right);
+        return Math.max(left, right);
+    }
+
+    public static void main(String[] args) {
+        Solution_1 solution = new Solution_1();
+
     }
 }
