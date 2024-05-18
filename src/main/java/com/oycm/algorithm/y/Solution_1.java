fd1002228b1faec1784bf0cce5683c843a4f006b
oycm <1164864987@qq.com>
2024-05-18 15:59:26 +0800

    y-968.监控二叉树：解释为什么会整形溢出

--- src/main/java/com/oycm/algorithm/y/Solution_1.java
+++ src/main/java/com/oycm/algorithm/y/Solution_1.java
@@ -11,13 +11,14 @@ public class Solution_1 {
      * @return
      */
     public int minCameraCover(TreeNode root) {
-        int[] ans = dfs(root);
+        int[] ans = dfs_simple(root);
         return Math.min(ans[0], ans[2]);
     }
 
     // int[0] 当前监控; int[1] 父节点监控; int[2] 至少一个子节点监控
     public int[] dfs(TreeNode node) {
-        if (node == null) return new int[] {10000, 0 ,0};
+        // 为什么为Integer.MAX_VALUE会溢出? 因为到空节点时 当前节点的l[0] r[0] 都会为最大值,在计算黄色时l[0] + r[0] 就溢出了
+        if (node == null) return new int[] {Integer.MAX_VALUE / 2, 0 ,0};
         int[] l = dfs(node.left);
         int[] r = dfs(node.right);
 
@@ -29,12 +30,21 @@ public class Solution_1 {
 
     // 计算公式优化
     public int[] dfs_simple(TreeNode node) {
-        if (node == null) return new int[] {10000, 0, 0};
+        // 这里在优化之后，就不会出现溢出了
+        if (node == null) return new int[] {Integer.MAX_VALUE, 0, 0};
         int[] l = dfs_simple(node.left);
         int[] r = dfs_simple(node.right);
         int chose = Math.min(l[0], l[1]) + Math.min(r[0], r[1]) + 1;
         int fChose = Math.min(l[0], l[2]) + Math.min(r[0], r[2]);
         int cChose = fChose + Math.max(0, Math.min(l[0] - l[2], r[0] - r[2]));
         return new int[] {chose, fChose, cChose};
+    }
+
+    public static void main(String[] args) {
+        Solution_1 solution = new Solution_1();
+        TreeNode root = new TreeNode(0, new TreeNode(0), null);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(0);
+        System.out.println(solution.minCameraCover(root));
     }
 }
