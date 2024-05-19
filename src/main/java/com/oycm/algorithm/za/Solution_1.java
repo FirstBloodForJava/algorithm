7b3b762226dde52bf8e04e859762da4be65c13b1
oycm <1164864987@qq.com>
2024-05-19 21:37:27 +0800

    za-239.滑动窗口最大值: 单调队列(从右到左递减)

--- src/main/java/com/oycm/algorithm/za/Solution_1.java
+++ src/main/java/com/oycm/algorithm/za/Solution_1.java
@@ -39,10 +39,34 @@ public class Solution_1 {
         return ans;
     }
 
+    public int[] method_2(int[] nums, int k) {
+        if (k == 1) return nums;
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        // deque 单调递增(从左到右)
+        for (int i = 0; i < n; i++) {
+            // 1.构建队列
+            while (!deque.isEmpty() && nums[i] >= nums[deque.getFirst()]){
+                deque.removeFirst();
+            }
+            deque.addFirst(i);
+
+            // 2.出队列
+            if (i - deque.getLast() > k -1) deque.removeLast();
+
+            // 3.记录答案
+            if (i - k + 1 >= 0) ans[i-k+1] = nums[deque.getLast()];
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         Solution_1 solution = new Solution_1();
         int[] nums = {1,3,-1,-3,5,3,6,7};
         int k = 3;
         System.out.println(Arrays.toString(solution.method_1(nums, k)));
+        System.out.println(Arrays.toString(solution.method_2(nums, k)));
     }
 }
