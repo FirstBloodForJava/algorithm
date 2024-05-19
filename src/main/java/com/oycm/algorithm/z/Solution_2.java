c20bbbb20cde93bd6729fe7a06111b87e7d046bb
oycm <1164864987@qq.com>
2024-05-19 09:58:26 +0800

    z-42.接雨水:单调栈(计算横向接水容量)

--- src/main/java/com/oycm/algorithm/z/Solution_2.java
+++ src/main/java/com/oycm/algorithm/z/Solution_2.java
@@ -1,5 +1,7 @@
 package com.oycm.algorithm.z;
 
+import java.util.Stack;
+
 public class Solution_2 {
 
     /**
@@ -9,6 +11,33 @@ public class Solution_2 {
      * @return
      */
     public int trap(int[] height) {
-        return 0;
+        // 利用单调栈，计算横向能接多少水
+        Stack<Integer> stack = new Stack<>();
+        int ans = 0;
+        for (int i = 0; i < height.length; i++) {
+            // 栈中还有元素，且大于等于栈顶元素
+            while (!stack.isEmpty() && height[i] >= height[stack.peek()]) {
+                // 栈顶的高
+                Integer pop = stack.pop();
+                if (stack.isEmpty()) {
+                    // 没有下一个元素，不能接水
+                    break;
+                }
+                // 栈顶下一个元素的高
+                Integer left = stack.peek();
+                int h = Math.min(height[left], height[i]) - height[pop];
+                int w = i - left - 1;
+                ans += h * w;
+            }
+            stack.push(i);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution_2 solution = new Solution_2();
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(solution.trap(height));
     }
 }
