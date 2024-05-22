485c5d37388164c79038ef1ef73aefa117d0553e
oycm <1164864987@qq.com>
2024-05-22 22:20:50 +0800

    a-16.最接近的三数之和：暴力解法

--- src/main/java/com/oycm/algorithm/a/Solution_4.java
+++ src/main/java/com/oycm/algorithm/a/Solution_4.java
@@ -12,4 +12,27 @@ public class Solution_4 {
     public int threeSumClosest(int[] nums, int target) {
         return 0;
     }
+
+    /**
+     * 暴力解法
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int method_1(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 3) return nums[0] + nums[1] + nums[2];
+        int ans = Integer.MAX_VALUE / 2;
+
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i+1; j < n-1; j++) {
+                for (int k = j+1; k < n; k++) {
+                    int temp = nums[i] + nums[j] + nums[k];
+                    if (Math.abs(temp-target) < Math.abs(ans-target)) ans = temp;
+                }
+            }
+        }
+
+        return ans;
+    }
 }
