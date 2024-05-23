363ad0e36ff33fdadca6d5e6b1ed98b877f09f00
ouyangcm <mingorg@163.com>
2024-05-23 16:35:14 +0800

    a-16.最接近的三数之和:排序+双指针

--- src/main/java/com/oycm/algorithm/a/Solution_4.java
+++ src/main/java/com/oycm/algorithm/a/Solution_4.java
@@ -1,5 +1,7 @@
 package com.oycm.algorithm.a;
 
+import java.util.Arrays;
+
 public class Solution_4 {
 
 
@@ -34,5 +36,43 @@ public class Solution_4 {
         }
 
         return ans;
+    }
+
+    /**
+     * 时间复杂度优化：优化思路——类似有序数组的求三数之和nums[i] + nums[j] + nums[k] = target, 固定nums[i]+nums[k] 在(i,k)之间找target-nums[i]-nums[k] = nums[j]
+     * 最接近的三数之和，转换成了求：固定nums[i]+nums[k] 在(i,k)之间找|target-nums[i]-nums[k] - nums[j]|的最小绝对值
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int method_2(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 3) return nums[0] + nums[1] + nums[2];
+        int ans = Integer.MAX_VALUE / 2;
+        Arrays.sort(nums);
+        for (int i = 0; i < n-2; i++) {
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int temp = nums[i] + nums[j] + nums[k];
+                if (temp > target) {
+                    k--;
+                }else {
+                    j++;
+                }
+                ans = Math.abs(temp-target) > Math.abs(ans-target) ? ans : temp;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,2,1,-4};
+        int target = 1;
+        Solution_4 solution = new Solution_4();
+
+        System.out.println(solution.method_1(nums, target));
+        System.out.println(solution.method_2(nums, target));
     }
 }
