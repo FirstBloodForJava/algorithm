b1774d167ba20d59e03eec6e51876aba4c29f187
ouyangcm <mingorg@163.com>
2024-05-28 17:18:15 +0800

    a-611.有效三角形的个数: 三角形判定条件补充

--- src/main/java/com/oycm/algorithm/a/Solution_7.java
+++ src/main/java/com/oycm/algorithm/a/Solution_7.java
@@ -4,6 +4,11 @@ public class Solution_7 {
 
     /**
      * 611.有效三角形的个数：https://leetcode.cn/problems/valid-triangle-number/description/
+     * 给定一个数组nums,nums[i],nums[j],nums[k],且i != j != k,组成三角形的个数
+     * a, b, c三条边组成三角形的条件:
+     *  a + b > c
+     *  a + c > b
+     *  b + c > a 如果a <= b <= c,则只需要满足a+b > c即可
      * @param nums
      * @return
      */
