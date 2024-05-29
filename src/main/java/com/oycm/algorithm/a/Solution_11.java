a9d5c0b6eed196547c5de1df0ed3eca5e2ef09d5
ouyangcm <mingorg@163.com>
2024-05-29 16:00:41 +0800

    a-2824.统计和小于目标的下标对数目:暴力解法

--- src/main/java/com/oycm/algorithm/a/Solution_11.java
+++ src/main/java/com/oycm/algorithm/a/Solution_11.java
@@ -8,9 +8,21 @@ public class Solution_11 {
      * 2824.统计和小于目标的下标对数目：https://leetcode.cn/problems/count-pairs-whose-sum-is-less-than-target/description/
      * @param nums
      * @param target
-     * @return
+     * @return 0 <= i < j < n,nums[i] + nums[j] < target的对数
      */
     public int countPairs(List<Integer> nums, int target) {
-        return 0;
+
+        int n = nums.size();
+        int ans = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (nums.get(i) + nums.get(j) < target) ans++;
+            }
+        }
+
+        return ans;
     }
+
+
 }
