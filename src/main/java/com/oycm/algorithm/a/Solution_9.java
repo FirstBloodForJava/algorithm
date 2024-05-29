396e169f64887ae8c9d01235a4515ce1396e12c1
ouyangcm <mingorg@163.com>
2024-05-29 13:04:47 +0800

    a-1512.好数对的数目:hash表

--- src/main/java/com/oycm/algorithm/a/Solution_9.java
+++ src/main/java/com/oycm/algorithm/a/Solution_9.java
@@ -1,13 +1,32 @@
 package com.oycm.algorithm.a;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution_9 {
 
     /**
      * 1512.好数对的数目：https://leetcode.cn/problems/number-of-good-pairs/description/
      * @param nums
-     * @return
+     * @return nums[i] == nums[j],且i<j,所有的对数
      */
     public int numIdenticalPairs(int[] nums) {
-        return 0;
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) ans += map.get(num);
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num)+1);
+            }else {
+                map.put(num, 1);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,1};
+        Solution_9 solution = new Solution_9();
+        System.out.println(solution.numIdenticalPairs(nums));
     }
 }
