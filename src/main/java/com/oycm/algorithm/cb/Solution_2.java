17d98d9db4e813d2b4ccd46b853018b30253ee0f
oycm <1164864987@qq.com>
2024-06-03 21:29:18 +0800

    cb-1493.删掉一个元素以后全为1的最长子数组

--- src/main/java/com/oycm/algorithm/cb/Solution_2.java
+++ src/main/java/com/oycm/algorithm/cb/Solution_2.java
@@ -3,9 +3,9 @@ package com.oycm.algorithm.cb;
 public class Solution_2 {
 
     /**
-     * 1493.删掉一个元素以后全为 1 的最长子数组：https://leetcode.cn/problems/longest-subarray-of-1s-after-deleting-one-element/description/
-     * @param nums
-     * @return
+     * 1493.删掉一个元素以后全为1的最长子数组：https://leetcode.cn/problems/longest-subarray-of-1s-after-deleting-one-element/description/
+     * @param nums 数组元素只能是0或1
+     * @return 在nums中删掉一个元素，返回最长只包含1的非空连续子数组的长度
      */
     public int longestSubarray(int[] nums) {
         int ans = 0;
