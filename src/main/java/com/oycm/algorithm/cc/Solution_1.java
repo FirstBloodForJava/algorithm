777f0c9c5053b10c590d088a25a38286461582e5
oycm <1164864987@qq.com>
2024-06-04 21:25:41 +0800

    cc-209.长度最小的子数组：不定长滑动窗口求最小(最短)

--- src/main/java/com/oycm/algorithm/cc/Solution_1.java
+++ src/main/java/com/oycm/algorithm/cc/Solution_1.java
@@ -6,10 +6,33 @@ package com.oycm.algorithm.cc;
  */
 public class Solution_1 {
 
+    /**
+     * 209.长度最小的子数组：https://leetcode.cn/problems/minimum-size-subarray-sum/description/
+     * @param target
+     * @param nums
+     * @return
+     */
     public int minSubArrayLen(int target, int[] nums) {
-        int ans = 0;
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
 
+            while (sum >= target) {
+                ans = Math.min(ans, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
 
-        return ans;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+    public static void main(String[] args) {
+        Solution_1 solution = new Solution_1();
+        int[] nums = {2,3,1,2,4,3};
+        int target = 7;
+        System.out.println(solution.minSubArrayLen(target, nums));
     }
 }
