c171e28da834a63e3736a6355e45534a4f636b65
ouyangcm <mingorg@163.com>
2024-06-07 16:26:50 +0800

    cc-1574.删除最短的子数组使剩余数组有序

--- src/main/java/com/oycm/algorithm/cc/Solution_3.java
+++ src/main/java/com/oycm/algorithm/cc/Solution_3.java
@@ -5,10 +5,12 @@ public class Solution_3 {
     /**
      * 1574.删除最短的子数组使剩余数组有序：https://leetcode.cn/problems/shortest-subarray-to-be-removed-to-make-array-sorted/
      * @param arr
-     * @return 返回删除最短连续的子数组长度，使的数组使非递减的
+     * @return 返回删除最短连续的子数组长度，使得剩余数组是非递减的
      */
     public int findLengthOfShortestSubarray(int[] arr) {
-        int ans = Integer.MAX_VALUE;
+        int n = arr.length;
+        if (n <= 1) return 0;
+        int ans = n-1;
 
         return ans;
     }
