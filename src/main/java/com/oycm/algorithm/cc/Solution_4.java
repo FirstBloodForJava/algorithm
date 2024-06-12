da1f18ad992f7592640b870f406ee541f99a9804
oycm <1164864987@qq.com>
2024-06-12 21:04:51 +0800

    cc-76.最小覆盖子串: 不定长滑动窗口+时间复杂度优化

--- src/main/java/com/oycm/algorithm/cc/Solution_4.java
+++ src/main/java/com/oycm/algorithm/cc/Solution_4.java
@@ -4,6 +4,7 @@ public class Solution_4 {
 
     /**
      * 76.最小覆盖子串：https://leetcode.cn/problems/minimum-window-substring/
+     * 时间复杂度O(52t + s)
      * @param s
      * @param t
      * @return s的最小字串：需要包含t所有字符的(顺序没有要求，相同数量保持一致)
@@ -71,11 +72,71 @@ public class Solution_4 {
         return true;
     }
 
+
+    /**
+     * 优化时间复杂度为O(t + s)
+     * @param s
+     * @param t
+     * @return
+     */
+    public String optimizeMinWindow(String s, String t) {
+        int n = s.length();
+        String ans = "";
+        if (n < t.length()) {
+            return ans;
+        }
+        // 1.统计t中每个字母出现的次数countT
+        int[] countT = new int[128];
+
+        // 将t中不同字母的数量记为count
+        int count = 0;
+        for (char c : t.toCharArray()) {
+            ++countT[c];
+            if (countT[c] == 1) count++;
+        }
+
+
+        // 初始化 ansLeft = -1, ansRight = s.length
+        int ansLeft = -1;
+        int ansRight = n;
+
+        // 初始化left = 0,统计s中么个字母出现的次数countS,遍历s
+        int left = 0;
+        int[] countS = new int[128];
+        char[] charS = s.toCharArray();
+        for (int right = 0; right < n; right++) {
+            ++countS[charS[right]];
+            // 当遍历的s中的t的字母数量相等时,count--
+            if (countS[charS[right]] == countT[charS[right]]) count--;
+            // 遍历countS中t相关字母出现次数,如果出现次数都大于等于countT中出现的次数
+
+            // count == 0,代表是符合要求了,这时left可以右移
+            while (count == 0) {
+
+                // 如果right - left < ansRight - ansLeft 记录答案
+                if (right - left < ansRight - ansLeft) {
+                    ansLeft = left;
+                    ansRight = right;
+                }
+                // 在减少当前左边字符时,如果字母数量相同,删除这个字母则会导致不匹配,所有count要+1
+                if (countS[charS[left]] <= countT[charS[left]]) count++;
+                // left向右移动
+                --countS[charS[left]];
+                left++;
+            }
+        }
+
+        if (ansLeft < 0) return ans;
+
+        return s.substring(ansLeft, ansRight+1);
+    }
+
     public static void main(String[] args) {
         String s = "ADOBECODEBANC";
         String t = "ABC";
         Solution_4 solution = new Solution_4();
         System.out.println(solution.minWindow(s, t));
+        System.out.println(solution.optimizeMinWindow(s, t));
 
         System.out.println((int)'a');
         System.out.println((int)'Z');
