5c2032b8e4c4ec58f07996a485697b73b373b651
oycm <1164864987@qq.com>
2024-06-16 04:37:50 +0800

    ce.多指针滑动窗口-930.和相同的二元子数组: 分类处理+队列+滑动

--- src/main/java/com/oycm/algorithm/ce/Solution_1_930.java
+++ src/main/java/com/oycm/algorithm/ce/Solution_1_930.java
@@ -1,5 +1,8 @@
 package com.oycm.algorithm.ce;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Solution_1_930 {
 
     /**
@@ -10,7 +13,49 @@ public class Solution_1_930 {
      */
     public int numSubarraysWithSum(int[] nums, int goal) {
         int ans = 0;
+        // [left,right]数组和为goal,则left左边为0的和right右边为0的
+        int n = nums.length;
+
+        int left = 0;
+
+        if (goal == 0) {
+            for (int right = 0; right < n; right++) {
+                if (nums[right] == 1) {
+                    //ans += right - left; 这里不记录答案
+                    left = right + 1;
+                }else {
+                    ans += right - left + 1;
+                }
+            }
+        }else {
+            int temp = 0;
+            Deque<Integer> queue = new LinkedList<>();
+            int leftLength = 1;
+            for (int right = 0; right < n; right++) {
+                temp += nums[right];
+                queue.add(right);
+                if (temp > goal) {
+                    queue.removeFirst();
+                    leftLength = 1;
+                    temp--;
+                }
+                if (temp == goal) {
+                    while (nums[queue.getFirst()] != 1) {
+                        queue.removeFirst();
+                        leftLength++;
+                    }
+                    ans += leftLength;
+                }
+            }
+        }
 
         return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution_1_930 solution = new Solution_1_930();
+        int[] nums = {0,0,0,0,0,0,1,0,0,0};
+        int goal = 0;
+        System.out.println(solution.numSubarraysWithSum(nums, goal));
     }
 }
