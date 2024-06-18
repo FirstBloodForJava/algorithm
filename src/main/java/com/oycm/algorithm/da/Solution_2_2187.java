604cc6234aaa7572d1d5353fa420cb6b34d9b631
ouyangcm <mingorg@163.com>
2024-06-18 15:57:07 +0800

    d.二分查找求最小-2187.完成旅途的最少时间: 闭区间+(int类型溢出考虑)

--- src/main/java/com/oycm/algorithm/da/Solution_2_2187.java
+++ src/main/java/com/oycm/algorithm/da/Solution_2_2187.java
@@ -1,5 +1,7 @@
 package com.oycm.algorithm.da;
 
+import java.util.Arrays;
+
 public class Solution_2_2187 {
 
     /**
@@ -9,8 +11,44 @@ public class Solution_2_2187 {
      * @return 完成至少totalTrips趟需要花费的最少时间(公交车独立运行,同时进行旅途互不影响)
      */
     public long minimumTime(int[] time, int totalTrips) {
-        long ans = 0;
+        long ans = 1;
+        // 时间time以时间为下标,完成的趟数是一个非递减数组;如何确定时间的范围[1,?]?通过min(time[i])*totalTrips作为最大值
+        long min = Integer.MAX_VALUE;
+        for (int i : time) {
+            min = Math.min(i, min);
+        }
+
+        // 搜索范围[1, min*totalTrips],转换成在一个非递减数组中,查找第一次大于等于target的索引
+        long left = 1;
+        long right = min * totalTrips;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long trips = calculateTrips(time, mid);
+            if (trips >= totalTrips) {
+                right = mid - 1;
+                ans = mid;
+            }else {
+                left = mid + 1;
+            }
+        }
 
         return ans;
+    }
+
+    public long calculateTrips(int[] time, long currTime) {
+        long ans = 0;
+        for (int i : time) {
+            ans += currTime / i;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution_2_2187 solution = new Solution_2_2187();
+        int[] time = new int[10000];
+        Arrays.fill(time, 1);
+        int totalTrips = 10000000;
+
+        System.out.println(solution.minimumTime(time, totalTrips));
     }
 }
