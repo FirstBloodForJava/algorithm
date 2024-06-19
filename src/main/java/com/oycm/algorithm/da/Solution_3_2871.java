cc667987c97180c472b275b1d97fa812b78de9fe
ouyangcm <mingorg@163.com>
2024-06-19 15:01:25 +0800

    贪心:2871.将数组分割成最多数目的子数组

--- src/main/java/com/oycm/algorithm/da/Solution_3_2871.java
+++ src/main/java/com/oycm/algorithm/da/Solution_3_2871.java
@@ -10,6 +10,29 @@ public class Solution_3_2871 {
     public int maxSubarrays(int[] nums) {
         int ans = 0;
 
+        // i & j的结果肯定是小于等于min(i, j),所以当[0,n)的与运算结果大于0则意味着子数组只能是一个才能满足是子数组分数最小的条件(子数组的AND都是大于等于整个数组的AND)
+        // 要想有更多的子数组,就需要满足任意连续子数组的分数都是0
+        int s = nums[0];
+        int left = 1;
+        int right = nums.length;
+        while (left < right) {
+            if (s == 0) {
+                ans++;
+                s = nums[left];
+            }else {
+                s = s & nums[left];
+            }
+
+            left++;
+        }
+        if (s > 0 && ans == 0) return 1;
+        if (s == 0) ans++;
         return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution_3_2871 solution = new Solution_3_2871();
+        int[] nums = {1,0,2,0,1,2};
+        System.out.println(solution.maxSubarrays(nums));
     }
 }
