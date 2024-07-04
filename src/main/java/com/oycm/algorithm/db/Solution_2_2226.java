81eb10347f93ad81f155e3244df19d08fd335c60
ouyangcm <mingorg@163.com>
2024-07-04 17:04:19 +0800

    db.二分查找(求最大)-2226.每个小孩最多能分到多少糖果

--- src/main/java/com/oycm/algorithm/db/Solution_2_2226.java
+++ src/main/java/com/oycm/algorithm/db/Solution_2_2226.java
@@ -8,7 +8,7 @@ public class Solution_2_2226 {
 
     /**
      * 2226.每个小孩最多能分到多少糖果: https://leetcode.cn/problems/maximum-candies-allocated-to-k-children/
-     * 将这些糖果分配给k个小孩,使每个小孩分到相同数量的糖果,每个小孩可以至多拿走一对糖果,有些糖果可能不被分配
+     * 将这些糖果分配给k个小孩,使每个小孩分到相同数量的糖果,每个小孩可以至多拿走一堆糖果,有些糖果可能不被分配
      * @param candies 整数数组,元素表示大小为candies[i]的一堆糖果,可以建糖果分成任意数量的子堆,但无法再将两堆合并到一起  eg: {5,8,6}
      * @param k 小孩数量 eg: 3
      * @return eg: 5
