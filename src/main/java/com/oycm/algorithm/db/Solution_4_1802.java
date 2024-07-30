ac4b03e563f9ecc5252ce92d9c79ca8e2dfb8cf2
ouyangcm <mingorg@163.com>
2024-07-30 16:59:12 +0800

    db.二分查找(求最大)-1802.有界数组中指定下标处的最大值:描述修改

--- src/main/java/com/oycm/algorithm/db/Solution_4_1802.java
+++ src/main/java/com/oycm/algorithm/db/Solution_4_1802.java
@@ -8,9 +8,12 @@ public class Solution_4_1802 {
 
     /**
      * 1802.有界数组中指定下标处的最大值: https://leetcode.cn/problems/maximum-value-at-a-given-index-in-a-bounded-array/
-     * 构造一个满足一下体哦阿健的数组nums
-     * nums.length = n; nums[i](0<=i<n)是正整数; ans(num[i]-nums[i+1]) <= 1(0<=i<n-1)
-     * nums所有元素之和不超过maxSum; nums[index]的值被最大化
+     * 构造一个满足一下条件的数组nums
+     *  nums.length = n;
+     *  nums[i](0<=i<n)是正整数;
+     *  abs(num[i]-nums[i+1]) <= 1(0<=i<n-1)
+     *  nums所有元素之和不超过maxSum;
+     *  nums[index]的值被最大化
      * @param n 构造的数组长度; eg: 4
      * @param index 数组中的最大值; eg: 2
      * @param maxSum 构造数组的阈值; eg: 6
