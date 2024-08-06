7774f72299258226fcaae0dd1e6af77d330e3797
ouyangcm <mingorg@163.com>
2024-08-06 16:59:24 +0800

    dd.最小化最大值-2439.最小化数组中的最大值

---
+++ src/main/java/com/oycm/algorithm/dd/Solution_5_2439.java
@@ -0,0 +1,17 @@
+package com.oycm.algorithm.dd;
+
+/**
+ * @author ouyangcm
+ * create 2024/8/6 16:57
+ */
+public class Solution_5_2439 {
+
+    /**
+     * 2439.最小化数组中的最大值：https://leetcode.cn/problems/minimize-maximum-of-array/
+     * @param nums
+     * @return
+     */
+    public int minimizeArrayValue(int[] nums) {
+        return 0;
+    }
+}
