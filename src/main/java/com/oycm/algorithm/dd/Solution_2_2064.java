5c9099bb9a5556e42796a0548c4a37c96409450a
ouyangcm <mingorg@163.com>
2024-08-06 16:51:32 +0800

    dd.最小化最大值-2064.分配给商店的最多商品的最小值

---
+++ src/main/java/com/oycm/algorithm/dd/Solution_2_2064.java
@@ -0,0 +1,23 @@
+package com.oycm.algorithm.dd;
+
+/**
+ * @author ouyangcm
+ * create 2024/8/6 16:41
+ */
+public class Solution_2_2064 {
+
+    /**
+     * 1886
+     * 2064.分配给商店的最多商品的最小值：https://leetcode.cn/problems/minimized-maximum-of-products-distributed-to-any-store/description/
+     * @param n 整数,表示n间零售商店,
+     * @param quantities quantities.length表示m种产品,quantities[i]表示第i种商品的数目
+     * 将所有商品分配到零售商店,并遵守规则：
+     * 1.一间商店至多只能有一种商品,拥有的商品数量可以为任意将
+     * 2.分配后,每间商店都会被分配一定数目的商品(可能为0).x表示所有商店中分配商品数目的最大值，最小化分配任意商店商品数目的最大值
+     * @return 返回最小的x
+     */
+    public int minimizedMaximum(int n, int[] quantities) {
+
+        return 0;
+    }
+}
