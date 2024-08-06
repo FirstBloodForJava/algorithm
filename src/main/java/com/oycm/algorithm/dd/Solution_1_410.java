93ebd2a500fe13ddcb6e6ec10b23cec111b6569b
ouyangcm <mingorg@163.com>
2024-08-06 16:40:52 +0800

    dd.最小化最大值-410.分割数组的最大值

---
+++ src/main/java/com/oycm/algorithm/dd/Solution_1_410.java
@@ -0,0 +1,20 @@
+package com.oycm.algorithm.dd;
+
+/**
+ * @author ouyangcm
+ * create 2024/8/6 16:33
+ */
+public class Solution_1_410 {
+
+    /**
+     * 410.分割数组的最大值：https://leetcode.cn/problems/split-array-largest-sum/description/
+     * @param nums 非负整数
+     * @param k 整数k
+     * @return 需要将这个数组分成k个非空的连续子数组,使得这k个子数组各自和的最大值最小
+     */
+    public int splitArray(int[] nums, int k) {
+        int ans = 0;
+
+        return ans;
+    }
+}
