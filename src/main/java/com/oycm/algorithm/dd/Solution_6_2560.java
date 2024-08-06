2c6daa0efe055e64618a49e1f4f8223cc44f2319
ouyangcm <mingorg@163.com>
2024-08-06 17:02:06 +0800

    dd.最小化最大值-2560.打家劫舍IV

---
+++ src/main/java/com/oycm/algorithm/dd/Solution_6_2560.java
@@ -0,0 +1,19 @@
+package com.oycm.algorithm.dd;
+
+/**
+ * @author ouyangcm
+ * create 2024/8/6 16:59
+ */
+public class Solution_6_2560 {
+
+    /**
+     * 2081
+     * 2560.打家劫舍IV：https://leetcode.cn/problems/house-robber-iv/
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int minCapability(int[] nums, int k) {
+        return 0;
+    }
+}
