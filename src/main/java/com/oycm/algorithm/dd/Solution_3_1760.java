9f92b18dd6c8bb5a0c640ef31a5fe50fdcc68a74
ouyangcm <mingorg@163.com>
2024-08-06 16:54:13 +0800

    dd.最小化最大值-1760.袋子里最少数目的球

---
+++ src/main/java/com/oycm/algorithm/dd/Solution_3_1760.java
@@ -0,0 +1,19 @@
+package com.oycm.algorithm.dd;
+
+/**
+ * @author ouyangcm
+ * create 2024/8/6 16:52
+ */
+public class Solution_3_1760 {
+
+    /**
+     * 1760.袋子里最少数目的球：https://leetcode.cn/problems/minimum-limit-of-balls-in-a-bag/description/
+     * @param nums
+     * @param maxOperations
+     * @return
+     */
+    public int minimumSize(int[] nums, int maxOperations) {
+
+        return 0;
+    }
+}
