020281383095bc78fa0a9407e2e354c1b66d507e
ouyangcm <mingorg@163.com>
2024-08-06 16:33:25 +0800

    dc.计算间接结果-3143.正方形中的最多点数

---
+++ src/main/java/com/oycm/algorithm/dc/Solution_1_3143.java
@@ -0,0 +1,22 @@
+package com.oycm.algorithm.dc;
+
+/**
+ * @author ouyangcm
+ * create 2024/8/6 16:22
+ */
+public class Solution_1_3143 {
+
+
+    /**
+     * 1600
+     * 3143.正方形中的最多点数：https://leetcode.cn/problems/maximum-points-inside-the-square/description/
+     * @param points 二维数组,points[i]表示第i个点的坐标(s.length == points.length)
+     * @param s
+     * @return 合法正方形中可以包含的最多点数（正方形的中心在(0,0),所有的边平行于坐标轴,且正方形内不存在标签相同的两个点,这个正方形是合法正方形）,返回合法正方形中可以包含的最多点数
+     */
+    public int maxPointsInsideSquare(int[][] points, String s) {
+        int ans = 0;
+
+        return ans;
+    }
+}
