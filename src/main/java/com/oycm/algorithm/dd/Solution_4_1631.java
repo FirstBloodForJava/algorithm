271606fbebb919f7e2511a3df246ad54f2f96054
ouyangcm <mingorg@163.com>
2024-08-06 16:57:51 +0800

    dd.最小化最大值-1631.最小体力消耗路径

---
+++ src/main/java/com/oycm/algorithm/dd/Solution_4_1631.java
@@ -0,0 +1,18 @@
+package com.oycm.algorithm.dd;
+
+/**
+ * @author ouyangcm
+ * create 2024/8/6 16:54
+ */
+public class Solution_4_1631 {
+
+    /**
+     * 1631.最小体力消耗路径：https://leetcode.cn/problems/path-with-minimum-effort/
+     * @param heights
+     * @return
+     */
+    public int minimumEffortPath(int[][] heights) {
+        return 0;
+    }
+
+}
